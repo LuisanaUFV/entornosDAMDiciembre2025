78498f3cae2c0d72f1136cf9ffc8d3a88b535bc7
LuisanaUFV <122102652+LuisanaUFV@users.noreply.github.com>
2025-12-17 20:07:23 +0100

    Se lee también el plus

--- src/ejercicio01/Principal.java
+++ src/ejercicio01/Principal.java
@@ -7,9 +7,12 @@ public class Principal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int antiguedad = 0;
+		int plus = 0;
 		System.out.println("Introduce antiguedad:");
 		antiguedad = sc.nextInt();
-		Salario salario = new Salario(antiguedad, 200);
+		System.out.println("Introduce plus:");
+		plus = sc.nextInt();
+		Salario salario = new Salario(antiguedad, plus);
 		System.out.println("Salario final: " + salario.calcularSalarioFinal());
 		
 		sc.close();
